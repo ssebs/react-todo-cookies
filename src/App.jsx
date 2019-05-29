90263c7d5e215c22fedf066637ebe804ebd06539
Sebastian Safari <ssebs@ymail.com>
2019-05-29 14:37:01 -0700

    base content

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,10 @@ import React from "react";
 const App = () => {
     return (
         <div>
-            <h1>App</h1>
+            <h1>React Todo Cookies</h1>
+            <p>
+                This is a basic todo app that will be saved in your browser's "Cookies" (Actually localStorage)
+            </p>
         </div>
     );
 };
