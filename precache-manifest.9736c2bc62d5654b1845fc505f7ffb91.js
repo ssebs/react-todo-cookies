9607fdb8172e0bd9d16dbb41847723930e17d87e
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-05-29 22:12:08 +0000

    Deploy ssebs/react-todo-cookies to github.com/ssebs/react-todo-cookies.git:gh-pages

---
+++ precache-manifest.9736c2bc62d5654b1845fc505f7ffb91.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1381d2dd209f74631a0f54fd42ddc3ea",
+    "url": "/react-todo-cookies/index.html"
+  },
+  {
+    "revision": "2315f74ce3c99316133d",
+    "url": "/react-todo-cookies/static/js/2.3310f33a.chunk.js"
+  },
+  {
+    "revision": "0b94238eb5dc180ecaf7",
+    "url": "/react-todo-cookies/static/js/main.b1efa08a.chunk.js"
+  },
+  {
+    "revision": "4c3f7551c35c74971c50",
+    "url": "/react-todo-cookies/static/js/runtime~main.4b746fd5.js"
+  }
+]);
